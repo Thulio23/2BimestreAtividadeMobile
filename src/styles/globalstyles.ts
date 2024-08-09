08edbe470cff4e840d93779802566524aa974391
Thulio23 <126250806+Thulio23@users.noreply.github.com>
2024-08-08 22:27:47 -0300

    Update globalstyles.ts

--- src/styles/globalstyles.ts
+++ src/styles/globalstyles.ts
@@ -3,10 +3,10 @@ import {StyleSheet} from "react-native"
 export const colors={
     black: '#000',
     white: '#fff',
-    primary: '#37474F',
-    secondary: '#C51162',
-    gray: '#D9D9D9',
-    third: '#D9D9D9',
+    primary: '#262323',
+    secondary: '#751212',
+    gray: '#EAE6E6',
+    third: '#EAE6E6',
 }
 
 export const styleContainer = StyleSheet.create({
